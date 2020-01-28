fc13ec70bc2e175786872fb74310fba32436c5ef
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-01-28 07:59:07 -0300

    numerso

--- fase03/ex05/acharNumero.js
+++ fase03/ex05/acharNumero.js
@@ -1,3 +1,10 @@
-function  acharNumero() {
-    
+function acharNumero(n) {
+        if (n < 5)
+            return 'mercurio';
+        if (n <= 10)
+            return 'venus';
+        if (n <= 100)
+            return 'terra';
+        else
+            return 'marte';
 }
