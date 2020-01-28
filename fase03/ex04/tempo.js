5e3936b23f56292be453c4bc83fc14160fd6bdc7
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-01-28 07:55:12 -0300

    tempo

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -1,3 +1,18 @@
-function tempo() {
+function tempo(t1, t2, unidade) {
+    const date1 = new Date(t1).valueOf();
+    const date2 = new Date(t2).valueOf();
+    const diff = Math.abs(date1 - date2);
     
+    const dias = 1000 * 60 * 60 * 24;
+    const mes = dias * 30;
+    const ano = mes * 12;
+    
+    switch (unidade) {
+        case 'dia':
+            return Math.round(diff / dias);
+        case 'mês':
+            return Math.round(diff / mes);
+        case 'ano':
+            return Math.round(diff / ano);
+    }
 }
