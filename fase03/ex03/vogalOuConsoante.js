9cc9746d635f6218332a0608a010a3c32c238d4b
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-01-28 07:50:09 -0300

    vogalconsoante

--- fase03/ex03/vogalOuConsoante.js
+++ fase03/ex03/vogalOuConsoante.js
@@ -1,3 +1,9 @@
-function vogalOuConsoante() {
-    
+function vogalOuConsoante(letra) {
+    const min = letra.toLowerCase();
+    const letras = 'aeiou';
+    if (letras.includes(min)) {
+        return 'vogal';
+    } else {
+        return 'consoante';
+    }
 }
