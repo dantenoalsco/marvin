c28b43e2d4a645c7f7f4e2c443604a20feeeda45
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 10:10:24 -0300

    f1

--- fase04/ex01/ateZero.js
+++ fase04/ex01/ateZero.js
@@ -1,3 +1,13 @@
-function ateZero() {
-    
+function ateZero(num) {
+    let i = 0;
+    if (num < 0) {
+        while (num <= 0) {
+            console.log(num++);
+        }
+    } else {
+        let i = 0;
+        while (num >= i) {
+            console.log(i++);
+        }
+    }
 }
