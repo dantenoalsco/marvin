5bdb271e86d3497037250b09557d021e5ddabc62
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 09:57:16 -0300

    enum

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,16 +1,6 @@
-function fizzbuzz(min, max) {
-    let i = min;
-    while (i <= max) {
-        if (i % 15 === 0) {
-            console.log(i + " FizzBuzz");
-        } else if (i % 5 === 0) {
-            console.log(i + " Buzz");
-        } else if (i % 3 === 0) {
-            console.log(i + " Fizz");
-        } else {
-            console.log(i);
-        }
-        i++;
+function entreNumeros(min, max) {
+    while (min <= max) {
+        console.log(min);
+        min++;
     }
 }
-
