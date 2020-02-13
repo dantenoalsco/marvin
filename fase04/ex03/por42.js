726d37fdf693d1a830e41bde4a92edbe10e93b76
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 11:35:15 -0300

    f3

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,4 +1,10 @@
-function por43() {
-    
+function por42(n1, n2) {
+    const target = n1 + 84;
+    const round = Math.floor(target / 42) * 42;
+    if (round < n2) {
+        return round;
+    } else {
+        console.log("Não encontrado");
+        return false;
+    }
 }
-
