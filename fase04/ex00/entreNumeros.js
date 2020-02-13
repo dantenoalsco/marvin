6482c80a829016ee2124167815d8a8de3c1471f8
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 09:14:29 -0300

    fb

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -2,14 +2,15 @@ function fizzbuzz(min, max) {
     let i = min;
     while (i <= max) {
         if (i % 15 === 0) {
-            console.log(i + " FizzBuzz")
+            console.log(i + " FizzBuzz");
         } else if (i % 5 === 0) {
             console.log(i + " Buzz");
         } else if (i % 3 === 0) {
-            console.log(1 + " Fizz");
+            console.log(i + " Fizz");
         } else {
             console.log(i);
         }
         i++;
     }
 }
+
