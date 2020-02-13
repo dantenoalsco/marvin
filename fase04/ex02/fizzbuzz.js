66b942bee9d4bc0a0c87617306af7ef154e064d3
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 08:59:58 -0300

    fb

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(min, max) {
+    let i = min;
+    while (i <= max) {
+        if (i % 15 === 0) {
+            console.log(i + " FizzBuzz");
+        } else if (i % 5 === 0) {
+            console.log(i + " Buzz");
+        } else if (i % 3 === 0) {
+            console.log(1 + " Fizz");
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+}
