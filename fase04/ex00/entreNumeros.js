357c7243c9ae5a5560a808d7e8929d7ab228d021
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 11:41:16 -0300

    ficx

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,6 +1,11 @@
-function entreNumeros(min, max) {
-    while (min <= max) {
-        console.log(min);
-        min++;
+function por42(n1, n2) {
+    let num = n1 + 42;
+    while (num <= n2) {
+        if (num % 42 === 0) {
+            return num;
+        }
+        num++;
     }
+    console.log("Não encontrado");
+    return false;
 }
