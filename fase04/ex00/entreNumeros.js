ec6e195c4653ea0ea9e58380fe322c9a5bdb1fb9
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2020-02-13 11:43:03 -0300

    fucking fix

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,11 +1,5 @@
-function por42(n1, n2) {
-    let num = n1 + 42;
-    while (num <= n2) {
-        if (num % 42 === 0) {
-            return num;
-        }
-        num++;
+function entreNumeros(n1, n2) {
+    while (n1 <= n2) {
+        console.log(n1++);
     }
-    console.log("Não encontrado");
-    return false;
 }
